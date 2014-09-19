10ef94aa05c7f7f946092e1d6acc1a4aa680aedc
pacho950609 <franciscoricaurte-95@hotmail.com>
2014-09-18 23:22:44 -0500

    :D conexion a la base de datos hecha

---
+++ WebApplication1/src/java/Mundo/Intermediario.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mundo;
+
+/**
+ *
+ * @author GLORIA AZUCENA
+ */
+public class Intermediario {
+    
+  private String nombre ;
+  private String registroSuperInte;
+  private String ciudad ;
+  private String telefono ; 
+  private String direccion ;
+  private String nombreRepLegal;
+  private String ccRepLegal;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
