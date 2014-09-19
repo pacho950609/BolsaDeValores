e15b6c3c18d34207eaffad7952f0c62d4f432e6c
pacho950609 <franciscoricaurte-95@hotmail.com>
2014-09-19 00:24:24 -0500

    clase oferente

--- WebApplication1/src/java/Mundo/Oferente.java
+++ WebApplication1/src/java/Mundo/Oferente.java
@@ -12,15 +12,122 @@ package Mundo;
 public class Oferente 
 {
     
+    //ATRIBUTOS
+    
+    //nombre
     private String nombre ;
+    //ciudad
     private String ciudad ;
+    //telefono
     private String telefono ; 
+    //direccion
     private String direccion ;
+    //nombre representante legal
     private String nombreRepLegal;
+    //cedula del representante legal
     private String ccRepLegal;
+    //intermediario elegido
+    private Intermediario intermediario ;
     
     
     
+   public Oferente(String pNombre , String pRegistro , String pCiudad,String pTelefono,String pDireccion  ,String pnombreRep , String pccRep)
+    {
+        
+        nombre = pNombre ;
+        ciudad=pCiudad ;
+        telefono = pTelefono;
+        direccion=pDireccion;
+        nombreRepLegal=pnombreRep;
+        ccRepLegal=pccRep;
+        
+        
+    }
+    
+    
+    public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+
+	public String getNombreRepLegal() {
+		return nombreRepLegal;
+	}
+
+
+
+	public void setNombreRepLegal(String nombreRepLegal) {
+		this.nombreRepLegal = nombreRepLegal;
+	}
+
+
+
+	public String getCcRepLegal() {
+		return ccRepLegal;
+	}
+
+
+
+	public void setCcRepLegal(String ccRepLegal) {
+		this.ccRepLegal = ccRepLegal;
+	}
+
+
+
+	public Intermediario getIntermediario() {
+		return intermediario;
+	}
+
+
+
+	public void setIntermediario(Intermediario intermediario) {
+		this.intermediario = intermediario;
+	}
+
+
     
     
     
