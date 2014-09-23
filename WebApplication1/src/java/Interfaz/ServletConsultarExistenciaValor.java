2d763f8560b05be9fc9979415576ee6f331bf8e0
pacho950609 <franciscoricaurte-95@hotmail.com>
2014-09-23 15:01:21 -0500

    hecha interfaz consulta por valores

---
+++ WebApplication1/src/java/Interfaz/ServletConsultarExistenciaValor.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaz;
+
+import Conexion.conexionDB;
+import Mundo.OperacionEsperaPrim;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author GLORIA AZUCENA
+ */
+public class ServletConsultarExistenciaValor extends HttpServlet{
+    
+    
+    	
+		
+		protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
+	    {
+	        // Maneja el GET y el POST de la misma manera
+			
+                       PrintWriter respuesta = response.getWriter() ;
+                       String consultaNit = request.getParameter( "nit" );
+                             
+                                  conexionDB x = new conexionDB();
+                                  
+                                  if(consultaNit!=null) 
+                                  {
+                                      
+                                  }
+	    }
+		/**
+	     * Maneja un pedido POST de un cliente
+	     * @param request Pedido del cliente
+	     * @param response Respuesta
+	     */
+	    protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
+	    {
+                           PrintWriter respuesta = response.getWriter() ;
+                        respuesta.write( "<html>\r\n" );
+                        
+                    try {
+                          OperacionEsperaPrim x = new OperacionEsperaPrim(90);
+                        respuesta.write("Bien"+ x.toString());
+                       
+                    } catch (Exception ex) {
+                         
+                         respuesta.write("Mal"+ex.getMessage());
+                    }
+                     respuesta.write( "</html>\r\n" );
+	    	
+	    }
+		
+		
+		
+		
+	
+    
+    
+    
+    
+}
